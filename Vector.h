62eb0a80f77fb79e27aa7d949d8c4a22a33b3964
Muuuush <2419108998@qq.com>
2024-11-25 21:15:06 +0800

    fix bugs

--- Vector.h
+++ Vector.h
@@ -129,7 +129,7 @@ namespace Muth
     }
 
     template <typename T, size_t n>
-    inline void Vector<T, n>::operator=(const Vector<T, n> &src)
+    inline Vector<T, n> &Vector<T, n>::operator=(const Vector<T, n> &src)
     {
         memcpy(elements, src.elements, n * sizeof(T));
     }
